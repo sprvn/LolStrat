150cf736f0034fac6b62656c02c2c39bc9125ddb
Kevin Gustafsson <kevin.gustafsson@gmail.com>
2016-06-03 16:56:32 +0000

    Moved credentials

--- lolstrat.py
+++ lolstrat.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template, redirect, request, session
 from flaskext.mysql import MySQL
+import csv
+
+csvFile = open('../creds.csv', "rb")
+reader = csv.reader(csvFile)
+
+line = next(reader)
+dbUser 	= line[0]
+dbPass 	= line[1]
+dbDb 	= line[2]
+dbHost 	= line[3]
+csvFile.close()
 
 app = Flask(__name__)
 app.debug = True
-app.config['MYSQL_DATABASE_USER'] = ''
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = ''
-app.config['MYSQL_DATABASE_HOST'] = ''
+app.config['MYSQL_DATABASE_USER'] = dbUser
+app.config['MYSQL_DATABASE_PASSWORD'] = dbPass
+app.config['MYSQL_DATABASE_DB'] = dbDb
+app.config['MYSQL_DATABASE_HOST'] = dbHost
 mysql = MySQL()
 mysql.init_app(app)
 
@@ -14,15 +25,22 @@ mysql.init_app(app)
 @app.route('/')
 @app.route('/party/<partyid>')
 def index(partyid=None):
-	yo = None
+	yo = 'None'
+	if session['nick'] == '':
+		print "yoyoyoyoy"
+	print "Session: %s" % session['nick']
 	if session.get('nick'):
-		cursor = mysql.connect().cursor()
-		cursor.execute("select riotID from summoners where summonerName='" + session['nick'] + "'")
-		sumID = cursor.fetchone()
-		sumID = sumID[0]
-		cursor.execute("select name from champions where id=17");
-		sumImage = cursor.fetchone()
-		sumImage = sumImage[0]
+		try:
+			cursor = mysql.connect().cursor()
+			cursor.execute("select riotID from summoners where summonerName='" + session['nick'] + "'")
+			sumID = cursor.fetchone()
+			if sumID:
+				sumID = sumID[0]
+			cursor.execute("select name from champions where id=17");
+			sumImage = cursor.fetchone()
+			sumImage = sumImage[0]
+		finally:
+			print "asd"
 	else:
 		sumImage = None
 	return render_template("index.html", partyid=partyid, nick=session['nick'], sumImage=sumImage)
